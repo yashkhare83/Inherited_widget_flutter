16804f9894fd6de43fb07a2c39f89b29a735d010
yashkhare83 <yashk@dewsolutions.in>
2024-07-17 17:55:22 +0530

    update fontstyle

--- main/lib/party_screen.dart
+++ main/lib/party_screen.dart
@@ -15,9 +15,20 @@ class _PartyScreenState extends State<PartyScreen> {
     return Scaffold(
       body: SafeArea(
           child: Center(
-        child: Text(
-            'Venue: ${partyDetails?.venue ?? 'No details' } & Date: ${partyDetails?.dateTime ?? DateTime.now()}'),
-      )),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Get data from inherited widget',style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                    'Venue: ${partyDetails?.venue ?? 'No details'} & Date: ${partyDetails?.dateTime.day ?? DateTime.now()}-${partyDetails?.dateTime.month ?? DateTime.now()}-${partyDetails?.dateTime.year ?? DateTime.now()}'),
+
+              ],
+            ),
+          )),
     );
   }
 }
